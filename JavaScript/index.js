9c6ad48abc1431214d9fcbd19716967b9f65d98c
Kvrathod348 <kvrathod348@gmail.com>
2024-08-06 13:58:53 +0530

    added function theme Changing btn

---
+++ JavaScript/index.js
@@ -0,0 +1,24 @@
+// Theme changing part
+const themeIcon = document.getElementById('theme-icon');
+const bodyElement = document.body;
+
+themeIcon.addEventListener('click' , () => {
+    if(bodyElement.classList.contains('bg-black')){
+      bodyElement.classList.remove('bg-black');
+      bodyElement.classList.add('bg-light');
+      themeIcon.classList.add('active-theme');
+      themeIcon.src = './Assets/icons/moon.png';
+      themeIcon.style.transition = '2s'
+      bodyElement.style.transition = '2s';
+    }
+    else{
+        bodyElement.classList.remove('bg-light');
+        bodyElement.classList.add('bg-black');
+        themeIcon.classList.add('active-theme');
+        themeIcon.src = './Assets/icons/sun.png';
+        themeIcon.style.transition = '2s'
+        bodyElement.style.transition = '2s';
+    }
+})
+
+const humBurger = document.getElementById('humburger')
