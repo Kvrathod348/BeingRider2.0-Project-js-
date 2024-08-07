174f0a4ddebaed762119459c5aa4c0ec1a4086e1
Kvrathod348 <kvrathod348@gmail.com>
2024-08-07 08:47:58 +0530

    changes file name and modified the html file and java file

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,14 +7,14 @@ themeIcon.addEventListener('click', () => {
     bodyElement.classList.remove('bg-black');
     bodyElement.classList.add('bg-light');
     themeIcon.classList.add('active-theme');
-    themeIcon.src = './Assets/icons/moon.png';
+    themeIcon.src = './assets/icons/moon.png';
     themeIcon.style.transition = '2s'
     bodyElement.style.transition = '2s';
   } else {
     bodyElement.classList.remove('bg-light');
     bodyElement.classList.add('bg-black');
     themeIcon.classList.add('active-theme');
-    themeIcon.src = './Assets/icons/sun.png';
+    themeIcon.src = './assets/icons/sun.png';
     themeIcon.style.transition = '2s'
     bodyElement.style.transition = '2s';
   }
@@ -33,14 +33,15 @@ hamburger.addEventListener('click', () => {
 // image slider
 
 const headImage = [
-  "./Assets/home/home-poster-1.jpg",
-  "./Assets/home/home-poster-2.jpg",
-  "./Assets/home/home-poster-3.jpg",
-  "./Assets/home/home-poster-4.jpg",
-  "./Assets/home/home-poster-5.jpeg"
+  "./assets/home/home-poster-1.jpg",
+  "./assets/home/home-poster-2.jpg",
+  "./assets/home/home-poster-3.jpg",
+  "./assets/home/home-poster-4.jpg",
+  "./assets/home/home-poster-5.jpeg"
 ];
 
 let headImageIndex = 0;
+
  const headImageElement = document.getElementById('head-poster');
 
  function prev(){
